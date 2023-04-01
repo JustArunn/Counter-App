feee672c255c953238d36119fb8cd71146178d8b
Arun Kumar <just.arunn@gmail.com>
2023-04-01 15:32:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let count = 0;
+
+document.getElementById("minus").onclick = function () {
+  count -= 1;
+  document.getElementById("show").innerHTML = count;
+  if (count > 0) {
+    document.getElementById("show").style.color = "green";
+  }
+  if (count < 0) {
+    document.getElementById("show").style.color = "red";
+  }
+};
+
+document.getElementById("plus").onclick = function () {
+  count += 1;
+  document.getElementById("show").innerHTML = count;
+  if (count > 0) {
+    document.getElementById("show").style.color = "green";
+  }
+  if (count < 0) {
+    document.getElementById("show").style.color = "red";
+  }
+};
+
+document.getElementById("resat").onclick = function () {
+  count = 0;
+  document.getElementById("show").innerHTML = count;
+  document.getElementById("show").style.color = "gray";
+};
